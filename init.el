42363e885f37c2a3f47eff05af4a4dbfe5d756c5
zeit <zei.t@qq.com>
2023-10-21 21:17:46 +0800

    update: packages

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  '(bookmark-default-file "~/.emacs.d/forOrgs/bookmarks")
  '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
-   '("ab2cbf30ab758c5e936b527377d543ce4927001742f79519b62c45ba9dd9f55e" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" default))
+   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "ab2cbf30ab758c5e936b527377d543ce4927001742f79519b62c45ba9dd9f55e" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" default))
  '(inhibit-startup-screen t)
  '(org-agenda-breadcrumbs-separator " > ")
  '(org-agenda-tags-column -60)
