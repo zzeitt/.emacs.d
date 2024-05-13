80e9ed35fb4e5d13ac8a9707f4dd4437fdfcd976
ztDell <>
2024-05-13 15:43:53 +0800

    add: make "startup-block" permanantly safe.

--- init.el
+++ init.el
@@ -62,7 +62,8 @@
  '(recentf-menu-filter 'recentf-arrange-by-dir)
  '(recentf-save-file "~/.emacs.d/forOrgs/recentf")
  '(safe-local-variable-values
-   '((eval add-hook 'before-save-hook 'time-stamp)
+   '((eval org-sbe "startup-block")
+     (eval add-hook 'before-save-hook 'time-stamp)
      (eval add-hook 'after-save-hook 'org-html-export-to-html nil 'make-it-local)
      (eval setq org-format-latex-options
            (plist-put org-format-latex-options :scale 1.2))
