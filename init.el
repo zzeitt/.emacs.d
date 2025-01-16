55d9173ebc2e58db347cf0208901157cb3fb67ed
ztDell <>
2025-01-16 17:19:21 +0800

    [func]add: zeit/org-paste-image-from-clipboard, based on powershell.

--- init.el
+++ init.el
@@ -2009,10 +2009,13 @@ append the cookies to the end of current headline."
 (defun zeit/org-ctrl-c-p ()
   "Map C-c p in different occasions."
   (interactive)
-  (when (org-at-table-p)
-    (call-interactively 'org-table-paste-rectangle)
-    )
-  )
+  (cond (;; Table
+         (org-at-table-p)
+         (call-interactively 'org-table-paste-rectangle))
+        (;; Normal/Insert/Visual
+         t
+         (call-interactively 'zeit/org-paste-image-from-clipboard))
+        ))
 
 (defun zeit/org-ctrl-c-space ()
   "Map C-c SPC in different occasions."
@@ -2029,6 +2032,25 @@ append the cookies to the end of current headline."
     (call-interactively 'org-table-blank-field)
     )
   )
+
+(defun zeit/org-paste-image-from-clipboard ()
+  "Take a screenshot into a time stamped unique-named file in the
+    same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (let* ((the-dir (file-name-directory (buffer-file-name)))
+         (attachments-dir (file-name-concat the-dir "_assets/"))
+         (jpg-file-name (format-time-string "%Y%m%d_%H%M%S.jpg"))
+         (jpg-path (file-name-concat attachments-dir jpg-file-name))
+         (jpg-relative-path (file-relative-name jpg-path the-dir)))
+    (if (not (file-directory-p attachments-dir)) (make-directory attachments-dir nil))
+    (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" jpg-path "',[System.Drawing.Imaging.ImageFormat]::Jpeg); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+    (insert
+     (format
+      "#+caption: %s\n#+attr_html: :width 80%%\n[[file:%s]]"
+      jpg-file-name
+      jpg-relative-path))
+    (org-redisplay-inline-images)
+    ))
 
 ;; ----------------------- AsciiDoc Export Backend --------------------
 ;; (add-to-list 'load-path "~/.emacs.d/myscripts-dev/ox-asciidoc/")
